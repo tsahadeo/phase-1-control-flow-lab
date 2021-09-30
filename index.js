4b513a2e189ca6f21fcf7b881fbd8b92c5bd0190
Travis <tsahadeo@mac.com>
2021-09-30 12:01:17 -0400

    commit tree 3f3a6f600c704778b39400c5944e8320a1e0ed04

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+if (someValue === 199){
+  return 'This one is on me!'}
+  else if (someValue == 2001){
+    return 'I will gladly take your thirty bucks.'}
+  else (someValue > 2500)
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  return (someCity === ('NYC')) ? ('Ok, sounds good.') : ('No go.')
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'; break;
+      case 'not as generous':
+        return 'Thank you.'; break;
+        default:
+          return 'Bye.'; break;
+  }
 }
